9868afa7c57f52f93f38f2825c9bd394ad4fa13f
Aymane El Halkouj <aymel-ha@e1r6p12.1337.ma>
2025-12-05 21:17:17 +0100

    progress

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "pars.h"
+#include <stdio.h>
+
+
+int main()
+{
+    // t_list 
+}
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: aymel-ha <aymel-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 10:53:21 by aymel-ha          #+#    #+#             */
-/*   Updated: 2025/12/03 11:23:08 by aymel-ha         ###   ########.fr       */
+/*   Updated: 2025/12/05 21:16:45 by aymel-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void del_content(void *content)
 t_list *gahter_lines(int fd)
 {
     t_list *list_of_lines = NULL;
-    t_list *line;
+    t_list *node;
 
     char *line = "magic";
     while(line != NULL)
@@ -31,37 +31,50 @@ t_list *gahter_lines(int fd)
             ft_lstclear(&list_of_lines, del_content);
             return NULL;
         }
-        line = ft_lstnew(line);
-        ft_lstadd_back(&list_of_lines, line);
+        node = ft_lstnew(line);
+        ft_lstadd_back(&list_of_lines, node);
     }
     return list_of_lines;
 }
-int verify_line(char *line, t_list *clear)
+
+
+int check_line(char *line,  int *start_point, int *exit_point, int *collectables)
 {
-    int i = 0;
-    int start_point = 0;
-    int exit_point = 0;
-    int collectables = 0;
+    if(!(line[0]) == '1'  && line[ft_strlen(line)-1] == '1')
+        return 0;
+    int i = 1;
     while(line[i] != '\0')
     {
-        if(!ft_strchr(&line[0], '1') || !ft_strrchr(&line, '1'))
-        {
-            
-        }
+        if(line[i] == 'E')
+            (*exit_point) += 1;
+        if(line[i] == 'P')
+            (*start_point) += 1;
+        if(line[i] == 'C')
+            (*collectables) += 1;
+        if(line[i] == '0')
+            i++;
+        else
+            return 0;
     }
+    return 1;
 }
 
-char *check_lines(t_list *lines)
+int map_verify(int fd)
 {
-    t_list *copy_to_clear = lines;
     int start_point = 0;
     int exit_point = 0;
     int collectables = 0;
+    int validity;
+    t_list *lines = gahter_lines(fd);
+    t_list *another_head = lines;
     while(lines->content != NULL)
     {
-        
+        validity = check_line(lines->content,start_point,exit_point,collectables);
+        if(!validity)
+        return ft_lstclear(&another_head, del_content), 0;
+        lines = lines->next;
     }
-    
+    if(!(start_point == 1 && exit_point == 1 && collectables >= 1))
+        return 0;
+    return 1;
 }
-
-int map_verify();
